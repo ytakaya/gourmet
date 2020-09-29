0d10637bad4640fb7251f4c3d2d7360ac399df84
yamazooooe <mt.tky@icloud.com>
2020-09-30 02:44:16 +0900

    add config.go

--- config/config.go
+++ config/config.go
@@ -1,5 +1,12 @@
 package config
 
+import (
+	"log"
+	"os"
+
+	"gopkg.in/ini.v1"
+)
+
 type ConfigList struct {
 	ApiKey string
 }
@@ -7,5 +14,13 @@ type ConfigList struct {
 var Config ConfigList
 
 func init() {
+	cfg, err := ini.Load("config.ini")
+	if err != nil {
+		log.Printf("Failed to read file: %v", err)
+		os.Exit(1)
+	}
 
+	Config = ConfigList{
+		ApiKey: cfg.Section("hotpepper").Key("api_key").String(),
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module yamazooooe/gourmet-share
 
 go 1.15
+
+require gopkg.in/ini.v1 v1.61.0
