b3a405c8946951733323aa9d861def88f10a7e7e
yamazooooe <mt.tky@icloud.com>
2020-09-30 23:40:32 +0900

    add doRequest

--- hotpepper/hotpepper.go
+++ hotpepper/hotpepper.go
@@ -1,3 +1,50 @@
 package hotpepper
 
+import (
+	"bytes"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
+)
+
 const baseURL = "https://webservice.recruit.co.jp/hotpepper/"
+
+type APIClient struct {
+	key        string
+	httpClient *http.Client
+}
+
+func (api *APIClient) doRequest(method, urlPath string, query map[string]string, data []byte) (body []byte, err error) {
+	baseURL, err := url.Parse(baseURL)
+	if err != nil {
+		return
+	}
+	apiURL, err := url.Parse(urlPath)
+	if err != nil {
+		return
+	}
+	endpoint := baseURL.ResolveReference(apiURL).String()
+	log.Printf("action=doRequest endpoint=%s", endpoint)
+	req, err := http.NewRequest(method, endpoint, bytes.NewBuffer(data))
+	if err != nil {
+		return
+	}
+	q := req.URL.Query()
+	q.Add("key", api.key)
+	for key, value := range query {
+		q.Add(key, value)
+	}
+	req.URL.RawQuery = q.Encode()
+
+	resp, err := api.httpClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	body, err = ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	return body, nil
+}
